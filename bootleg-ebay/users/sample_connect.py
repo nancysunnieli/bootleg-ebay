6c2658a6924ca9e61cc6b3c1d86d9898ba8c93e5
JinLi711 <jinli7255@gmail.com>
2021-10-24 14:49:01 -0500

    upd sample_connect.py

--- bootleg-ebay/users/sample_connect.py
+++ bootleg-ebay/users/sample_connect.py
@@ -1,17 +1,3 @@
-# from mysql.connector import connect, Error
-
-# try:
-#     with connect(
-#         host="localhost:3306",
-#         user="%s" % ("root"),
-#         password="%s" % ("bootleg"),
-#         # user=input("Enter username: "),
-#         # password=getpass("Enter password: "),
-#     ) as connection:
-#         print(connection)
-# except Error as e:
-#     print(e)
-
 from getpass import getpass
 from mysql.connector import connect, Error
 
@@ -23,9 +9,18 @@ try:
         port="3306",
         user="%s" % ("root"),
         password="%s" % ("bootleg"),
+        database="users",
         # user=input("Enter username: "),
         # password=getpass("Enter password: "),
     ) as connection:
+        select_movies_query = "SELECT * FROM users LIMIT 5"
+        with connection.cursor() as cursor:
+            cursor.execute(select_movies_query)
+            result = cursor.fetchall()
+
+        import pdb; pdb.set_trace()
+            # for row in result:
+            #     print(row)
         print(connection)
 except Error as e:
     print(e)
