661be56bf7d8053cf00f9f2eda5bac66438bf7a9
JinLi711 <jinli7255@gmail.com>
2021-10-24 14:14:45 -0500

    sample connect

---
+++ bootleg-ebay/users/sample_connect.py
@@ -0,0 +1,31 @@
+# from mysql.connector import connect, Error
+
+# try:
+#     with connect(
+#         host="localhost:3306",
+#         user="%s" % ("root"),
+#         password="%s" % ("bootleg"),
+#         # user=input("Enter username: "),
+#         # password=getpass("Enter password: "),
+#     ) as connection:
+#         print(connection)
+# except Error as e:
+#     print(e)
+
+from getpass import getpass
+from mysql.connector import connect, Error
+
+try:
+    with connect(
+        # host="mysql:8://db",
+        # host="mysql",
+        host="localhost",
+        port="3306",
+        user="%s" % ("root"),
+        password="%s" % ("bootleg"),
+        # user=input("Enter username: "),
+        # password=getpass("Enter password: "),
+    ) as connection:
+        print(connection)
+except Error as e:
+    print(e)
