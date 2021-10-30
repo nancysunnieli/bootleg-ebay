8511cf303012bc91d0f81f2314d33da05b570e7a
JinLi711 <jinli7255@gmail.com>
2021-10-30 11:20:58 -0500

    added an sql file for initializing the user table

---
+++ bootleg-ebay/users/init_table.sql
@@ -0,0 +1,13 @@
+-- for reference
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `username` varchar(100) NOT NULL UNIQUE,
+  `password` varchar(100) NOT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `money` float DEFAULT 0,
+  `suspended` boolean DEFAULT 0,
+  `is_admin` boolean DEFAULT 0,
+  PRIMARY KEY (`id`)
+);
