8b763248047c72235c13e29a3f469729d8e07871
JinLi711 <jinli7255@gmail.com>
2021-10-30 17:35:43 -0500

    upd auctions.py

--- bootleg-ebay/auctions/auction.py
+++ bootleg-ebay/auctions/auction.py
@@ -1,16 +1,26 @@
 from typing import Any, Dict, Optional, Sequence
 import datetime
 
-AuctionID = str
-UserID = str
-DateTime = datetime.date
+AuctionID = int
+BidID = Optional[int]
+UserID = int
+Price = float
+DateTime = Optional[float]
 AuctionInfo = Dict
+
+def current_time():
+    return float(datetime.datetime.now().timestamp())
 
 class Bid:
     """Represents the bid made by a particular user
 
     """
-    def __init__(self, price: float, buyer_id: UserID, bid_time: DateTime) -> None:
+    def __init__(
+        self, 
+        bid_id: BidID, 
+        price: Price, 
+        buyer_id: UserID, 
+        bid_time: DateTime = None) -> None:
         """
 
         Args:
@@ -18,9 +28,21 @@ class Bid:
             buyer_id: Id of the buyer making the bid
             bid_time: The time made by the bid
         """
+        
+        assert price >= 0
+
+        self._bid_id = bid_id
         self._price = price
         self._buyer_id = buyer_id
+
+        if bid_time is None:
+            bid_time = current_time()
+
         self._bid_time = bid_time
+
+    @property
+    def bid_id(self):
+        return self._bid_id
 
     @property
     def price(self):
@@ -34,12 +56,35 @@ class Bid:
     def bid_time(self):
         return self._bid_time
 
+    @classmethod
+    def from_mongodb_fmt(cls, mongodb_data):
+        new_instance = cls(
+            bid_id=mongodb_data['_id'],
+            price=mongodb_data['price'],
+            buyer_id=mongodb_data['buyer_id'],
+            bid_time=mongodb_data['bid_time'])
+
+        return new_instance
+
+    def to_mongodb_fmt(self):
+        bid_info = {
+            'price': self.price,
+            'buyer_id': self.buyer_id,
+            'bid_time': self.bid_time
+        }
+
+        if self.bid_id is not None:
+            bid_info['_id'] = self.bid_id
+
+
+        return bid_info
+
+
 class Auction:
     """
     
     Args:
         auction_id: Unique id assigned to the auction
-        item: Item to be sold. Item Id and seller id should be attached to this
         bids: List of bids made 
         auction_info: Information regarding the auction
     """
@@ -47,15 +92,15 @@ class Auction:
         self, 
         auction_id: AuctionID, 
         # seller_id: UserID, 
-        item, 
         bids: Sequence[Bid] = [],
         auction_info: AuctionInfo = {}) -> None:
 
         self._auction_id = auction_id
         # self._seller_id = seller_id
-        self._item = item
         self._bids = bids
         self._auction_info = {
+            'item_id': None,
+
             'buy_now': None, # Bool
             'start_time': None, # Datetime
             'end_time': None, # Datetime
@@ -69,22 +114,36 @@ class Auction:
             'current_bid_price': 0.0, # float
             'latest_bid_time': None # Datetime
         }
+
+        for k in auction_info.keys():
+            if k not in self._auction_info:
+                raise ValueError('You cannot have key: {} for `aucton_info`'.format(k))
+
         self._auction_info.update(auction_info)
 
         for k, v in self._auction_info.items():
             assert v is not None
 
+    @classmethod
+    def from_mongodb_fmt(cls, mongodb_data):
+        bids = [Bid.from_mongodb_fmt(bid) for bid in mongodb_data['bids']]
+        auction_id = mongodb_data['_id']
+        del mongodb_data['bids']
+        del mongodb_data['_id']
+        
+        new_instance = cls(auction_id=auction_id, bids=bids, auction_info=mongodb_data)
+        return new_instance
+
+    def to_mongodb_fmt(self):
+        mongodb_dict = self.auction_info
+        mongodb_dict['_id'] = self.auction_id
+        mongodb_dict['bids'] = [b.to_mongodb_fmt() for b in self.bids]
+
+        return mongodb_dict
+
     @property
     def auction_id(self):
         return self._auction_id
-
-    # @property
-    # def seller_id(self):
-    #     return self.seller_id
-
-    @property
-    def item(self):
-        return self._item
 
     @property
     def bids(self):
@@ -102,7 +161,7 @@ class Auction:
         if auction_info['start_time'] is not None:
             raise ValueError('You cannot start an auction that has already been started.')
 
-        auction_info['start_time'] = datetime.date.today()
+        auction_info['start_time'] = current_time()
         auction_info['started'] = True
 
     def stop_auction(self) -> None:
@@ -113,17 +172,20 @@ class Auction:
         if auction_info['completed'] is True:
             raise ValueError('You cannot end an auction that already has been completed.')
 
-        auction_info['end_time'] = datetime.date.today()
+        auction_info['end_time'] = current_time()
         auction_info['completed'] = True
 
-    def place_bid(self, bid: Bid) -> None:
+    def place_bid(self, buyer_id, price) -> bool:
         """Place a bid to the auction
 
         Args:
             bid: A bid made by some user
+
+        Returns:
+            True if a bid was successfully placed. False if otherwise
         """
 
-
+        bid = Bid(bid_id=None, price=price, buyer_id=buyer_id)
         auction_info = self._auction_info
 
         if auction_info['started'] is False:
@@ -134,10 +196,15 @@ class Auction:
 
         if bid.price > auction_info['current_bid_price'] and bid.bid_time > auction_info['latest_bid_time']:
             self._bids.append(bid)
+            successful = True
+        else:
+            successful = False
 
         # end the auction if the bid price is greater than the maximum auction price
         if bid.price >= auction_info['max_auction_price']:
             self.stop_auction()
+
+        return successful
 
     def view_bids(self, buyer_id: Optional[UserID] = None) -> Sequence[Bid]:
         """View all the bids
@@ -158,12 +225,9 @@ class Auction:
         # This is logically equivalent to placing a bid with the price equal to the maximum auction bid price
         auction_info = self._auction_info 
         
-        bid = Bid(
-            price=auction_info['max_auction_price'],
-            buyer_id=buyer_id,
-            bid_time=datetime.date.today())
+        successful = self.place_bid(buyer_id, auction_info['max_auction_price'])
 
-        self.place_bid(bid)
+        return successful
 
             
 
