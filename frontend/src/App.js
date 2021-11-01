e28a83677bf131cb78fb402247b523e51c735811
yvesshum <yvesshum1210@gmail.com>
2021-11-01 17:56:48 -0500

    moved to bootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,10 @@
 import React, { useState, useEffect, useCallback } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-
+import NavBar from "react-bootstrap/NavBar";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import NavDropdown from "react-bootstrap/NavDropdown";
 import "./App.css";
 
 import Login from "./components/Login/Login";
@@ -11,106 +14,92 @@ import Profile from "./components/Profile/Profile";
 import AdminDashboard from "./components/AdminDashboard/AdminDashboard";
 
 import { logout } from "./slices/auth";
-
+import PrivateRoute from "./components/Routing/PrivateRouter";
+import { ROLE_USER, ROLE_ADMIN } from "./constants";
+const NotFound = () => {
+    return <h1>Not found</h1>;
+};
 const App = () => {
-  const [showAdminBoard, setShowAdminBoard] = useState(false);
+    const [showAdminBoard, setShowAdminBoard] = useState(false);
 
-  const { user: currentUser } = useSelector((state) => state.auth);
-  const dispatch = useDispatch();
+    const { currentUser } = useSelector((state) => state.auth);
+    const dispatch = useDispatch();
 
-  const logOut = useCallback(() => {
-    dispatch(logout());
-  }, [dispatch]);
+    const logOut = useCallback(() => {
+        dispatch(logout());
+    }, [dispatch]);
 
-  useEffect(() => {
-    if (currentUser) {
-      setShowAdminBoard(currentUser.roles.includes("ROLE_ADMIN"));
-    } else {
-      setShowAdminBoard(false);
-    }
+    useEffect(() => {
+        if (currentUser) {
+            setShowAdminBoard(currentUser.roles.includes("ROLE_ADMIN"));
+        } else {
+            setShowAdminBoard(false);
+        }
 
-    document.addEventListener("logout", () => {
-      logOut();
-    });
+        document.addEventListener("logout", () => {
+            logOut();
+        });
 
-    return () => {
-      document.removeEventListener("logout", () => {});
-    };
-  }, [currentUser, logOut]);
+        return () => {
+            document.removeEventListener("logout", () => {});
+        };
+    }, [currentUser, logOut]);
 
-  return (
-    <Router>
-      <div>
-        <nav className="navbar navbar-expand navbar-dark bg-dark">
-          <Link to={"/"} className="navbar-brand">
-            Bootleg Ebay
-          </Link>
-          <div className="navbar-nav mr-auto">
-            <li className="nav-item">
-              <Link to={"/home"} className="nav-link">
-                Home
-              </Link>
-            </li>
+    return (
+        <Router>
+            <div>
+                <NavBar bg="light" expand="lg">
+                    <Container>
+                        <NavBar.Brand href={"/home"}>Bootleg Ebay</NavBar.Brand>
+                        <NavBar.Toggle aria-controls="basic-navbar-nav" />
+                        <NavBar.Collapse id="basic-navbar-nav">
+                            <Nav className="me-auto">
+                                {currentUser && [
+                                    <Nav.Link href="/home">Home</Nav.Link>,
+                                    <Nav.Link href="/profile">Profile</Nav.Link>,
+                                ]}
+                                {currentUser ? (
+                                    <Nav.Link href="/logout">Logout</Nav.Link>
+                                ) : (
+                                    [
+                                        <Nav.Link href="/login">Login</Nav.Link>,
+                                        <Nav.Link href="/register">Register</Nav.Link>,
+                                    ]
+                                )}
+                                {showAdminBoard && <Nav.Link href="/admin">Admin</Nav.Link>}
+                            </Nav>
+                        </NavBar.Collapse>
+                    </Container>
+                </NavBar>
 
-            {showAdminBoard && (
-              <li className="nav-item">
-                <Link to={"/admin"} className="nav-link">
-                  Admin
-                </Link>
-              </li>
-            )}
-
-            {currentUser && (
-              <li className="nav-item">
-                <Link to={"/profile"} className="nav-link">
-                  Profile
-                </Link>
-              </li>
-            )}
-          </div>
-
-          {currentUser ? (
-            <div className="navbar-nav ml-auto">
-              <li className="nav-item">
-                <Link to={"/profile"} className="nav-link">
-                  {currentUser.username}
-                </Link>
-              </li>
-              <li className="nav-item">
-                <a href="/login" className="nav-link" onClick={logOut}>
-                  LogOut
-                </a>
-              </li>
+                <div className="container mt-3">
+                    <Switch>
+                        <Route exact path="/login" component={Login} />
+                        <Route exact path="/register" component={Register} />
+                        <PrivateRoute
+                            exact
+                            path={["/", "/home"]}
+                            component={Home}
+                            roles={[ROLE_USER]}
+                        />
+                        <PrivateRoute
+                            exact
+                            path="/profile"
+                            component={Profile}
+                            roles={[ROLE_USER]}
+                        />
+                        <PrivateRoute
+                            exact
+                            path="/admin"
+                            component={AdminDashboard}
+                            roles={[ROLE_ADMIN]}
+                        />
+                        <Route path="/" component={NotFound} />
+                    </Switch>
+                </div>
             </div>
-          ) : (
-            <div className="navbar-nav ml-auto">
-              <li className="nav-item">
-                <Link to={"/login"} className="nav-link">
-                  Login
-                </Link>
-              </li>
-
-              <li className="nav-item">
-                <Link to={"/register"} className="nav-link">
-                  Register
-                </Link>
-              </li>
-            </div>
-          )}
-        </nav>
-
-        <div className="container mt-3">
-          <Switch>
-            <Route exact path={["/", "/home"]} component={Home} />
-            <Route exact path="/login" component={Login} />
-            <Route exact path="/register" component={Register} />
-            <Route exact path="/profile" component={Profile} />
-            <Route path="/admin" component={AdminDashboard} />
-          </Switch>
-        </div>
-      </div>
-    </Router>
-  );
+        </Router>
+    );
 };
 
 export default App;
