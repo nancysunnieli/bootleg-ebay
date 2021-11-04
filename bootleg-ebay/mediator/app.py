740c2cb8b4fdace13c1dee49fdda745a67328e37
yvesshum <yvesshum1210@gmail.com>
2021-11-04 16:59:39 -0500

    app prefix blueprint import

--- bootleg-ebay/mediator/app.py
+++ bootleg-ebay/mediator/app.py
@@ -7,12 +7,15 @@ from flask import Flask, Response, request
 from flask_expects_json import expects_json
 from flask_cors import CORS
 
-# from items import * 
-
-from routes.users import users_api
+from routes import *
 
 app = Flask(__name__)
 app.register_blueprint(users_api, url_prefix='/users')
+app.register_blueprint(payments_api, url_prefix='/payments')
+app.register_blueprint(auctions_api, url_prefix='/auctions')
+app.register_blueprint(items_api, url_prefix='/items')
+app.register_blueprint(carts_api, url_prefix='/carts')
+
 
 
 socket_name = socket.gethostbyname(socket.gethostname())
---
+++ bootleg-ebay/mediator/routes/__init__.py
@@ -0,0 +1,5 @@
+from .items import items_api
+from .users import users_api
+from .auctions import auctions_api
+from .carts import carts_api
+from .payments import payments_api
