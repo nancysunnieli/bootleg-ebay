cb0174c8e59a96af7bb81f88cbd5f6d27b5afbb0
yvesshum <yvesshum1210@gmail.com>
2021-11-04 16:09:15 -0500

    fixed frontend login

--- frontend/src/services/user.service.js
+++ frontend/src/services/user.service.js
@@ -1,6 +1,6 @@
 import axios from "axios";
-import authHeader from "./auth-header";
 import { API_URL } from "../config";
+import AuthService from "./auth.service";
 
 export const getUserInfo = (user_id) => {
     return axios.post(API_URL + "/Users/ViewUser", {
@@ -13,9 +13,13 @@ export const suspendAccount = (user_id) => {
 };
 
 export const modifyProfile = (user_id, fields) => {
-    return axios.post(API_URL + "/Users/ModifyProfile", { user_id, fields });
+    return axios.post(API_URL + "/Users/ModifyProfile", { user_id, ...fields });
 };
 
-export const deleteAccount = (user_id) => {
-    return axios.post(API_URL + "/Users/DeleteAccount", { user_id });
+export const deleteAccount = async (user_id) => {
+    const response = await axios.post(API_URL + "Users/DeleteAccount", {
+        user_id,
+    });
+    AuthService.logout();
+    return response.data;
 };
