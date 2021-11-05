2de15ded81b760566a039dc110878bbf496b0ca1
JinLi711 <jinli7255@gmail.com>
2021-11-04 20:06:40 -0500

    added expected json for payments

--- bootleg-ebay/mediator/routes/payments.py
+++ bootleg-ebay/mediator/routes/payments.py
@@ -13,18 +13,39 @@ payments_api = Blueprint('payments', __name__)
 # getting IP Address of payments container
 # The following are functions for the payments microservice
 
+_payment_info_schema = {
+    'type': 'object',
+    'properties': {
+        'user_id': {'type': 'int'},
+        'card_number': {'type': 'int'},
+        'security_code': {'type': 'int'},
+        'expiration_date': {'type': 'string'}
+    },
+    'required': ['user_id', 'card_number', 'security_code', 'expiration_date']
+}
+
+_payment_id_schema = {
+    'type': 'object',
+    'properties': {
+        'payment_id': {'type': 'int'},
+    },
+    'required': ['payment_id']
+}
 
 @payments_api.route("create_payment_card", methods = ['POST'])
+@expects_json(_payment_info_schema)
 def create_payment_card():
     socket_url = ("http://" + PAYMENTS_SERVICE_HOST + PAYMENTS_PORT + "/create_payment_card")
     return get_and_post(socket_url)
 
 @payments_api.route("get_payment_card", methods = ['POST'])
+@expects_json(_payment_id_schema)
 def get_payment_card():
     socket_url = ("http://" + PAYMENTS_SERVICE_HOST + PAYMENTS_PORT + "/get_payment_card")
     return get_and_post(socket_url)
 
 @payments_api.route("delete_account", methods = ['POST'])
+@expects_json(_payment_id_schema)
 def payments_delete_account():
     socket_url = ("http://" + PAYMENTS_SERVICE_HOST + PAYMENTS_PORT + "/delete_account")
     return get_and_post(socket_url)
