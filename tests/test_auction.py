0aa9c6f66084511b2cf0d89dc482999d9eb28d46
JinLi711 <jinli7255@gmail.com>
2021-11-16 15:49:45 -0600

    changed auction duration from 4 to 10 for testing

--- tests/test_auction.py
+++ tests/test_auction.py
@@ -84,7 +84,7 @@ class TestAuction(TestCase):
         item_id = self._create_item()
 
         # duration of auction in terms of seconds
-        auction_duration = 4
+        auction_duration = 10
 
         # create auction successfully
         url = self.base_url + "auction"
